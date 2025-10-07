5e7128f39c1302cfef1cbd0ba64769735f45f664
Gustavo Piucco <gustavopiucco@gmail.com>
2025-10-07 15:09:25 +0200

    test webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -2,7 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const { id } = (await req.json()) as { id: string };
+    const data = await req.json();
+
+    console.log(data);
 
     return NextResponse.json({}, { status: 200 });
   } catch (err) {
